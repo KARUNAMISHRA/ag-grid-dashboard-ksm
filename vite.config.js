41d8c12120a7a9f7b4c9218fe991ad1faf11d010
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2026-08-26 10:01:21 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({ 
   plugins: [react()],
 })
